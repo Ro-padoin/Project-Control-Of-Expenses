2c14dc4ac2215563fdf1413cfc3ce70fcb33b47f
Rosalia Padoin Oliveira <rosalia@rosalia-VPCEH27FX>
2022-02-10 18:35:01 -0300

    feat: Requisito 6

--- src/components/TableExpense/index.js
+++ src/components/TableExpense/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class TableExpense extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <section>
         <table>
@@ -16,10 +19,46 @@ class TableExpense extends Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+          { expenses.map(({
+            id,
+            description,
+            tag,
+            method,
+            value,
+            currency,
+            exchangeRates }) => {
+            const cambioUtilizado = Number(exchangeRates[currency].ask).toFixed(2);
+            const valueExpense = Number(value).toFixed(2);
+            const moedaConversao = (exchangeRates[currency].name);
+            return (
+              <tr key={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>{ ` ${currency} ${valueExpense}` }</td>
+                <td>{ moedaConversao }</td>
+                <td>{ cambioUtilizado }</td>
+                <td>{ cambioUtilizado * valueExpense }</td>
+                <td>Real Brasileiro</td>
+                <td>
+                  <button type="button">Editar</button>
+                  <button type="button">Excluir</button>
+                </td>
+              </tr>
+            );
+          })}
         </table>
       </section>
     );
   }
 }
 
-export default TableExpense;
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+TableExpense.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.string),
+}.isRequired;
+
+export default connect(mapStateToProps)(TableExpense);
